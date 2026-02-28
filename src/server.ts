e46b1fd226ada51b2fbd2f38cc297322420eb565
jtranberg <jtranberg@hotmail.ca>
2026-02-28 15:10:58 -0800

    Initial TS syndicator standalone

--- src/server.ts
+++ src/server.ts
@@ -54,7 +54,7 @@ app.use(
     credentials: false,
   })
 );
-app.options("*", cors());
+app.options(/.*/, cors());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
